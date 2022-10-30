1683b10d8ff46c3c7a772a3341318e71f578de95
saloni shahi <salonishahi0005@gmail.com>
2022-10-30 15:54:42 +0530

    updated Assignment6

--- Assignment3/Assignment3/Program.cs
+++ Assignment3/Assignment3/Program.cs
@@ -1,142 +1,154 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
 
+namespace EmployeeManagementSystem
 
-namespace Program
 {
-    class Program
+
+    public interface IPrintable
+
     {
 
+        void DisplayEmployeeDetails();
 
+    }
 
+    public class Employee
 
-        class Employee
+    {
+
+        public int emp_id;
+
+        public string emp_name;
+
+        public float Salary;
+
+        public float GrossSalary;
+
+        public void GetSalaryWithEmployeeDetails()
+
         {
-            private int EmpNo;
-            private string EmpName;
-            private double Salary;
-            private double HRA;
-            private double TA;
-            private double DA;
-            private double PF;
-            private double TDS;
-            private double NetSalary;
-            private double GrossSalary;
 
-            public void setEmployee(int EmpNo, string EmpName, double Salary)
-            {
-                this.EmpNo = EmpNo;
-                int Eno = this.EmpNo;
-                this.EmpName = EmpName;
-                string Ena = this.EmpName;
-                this.Salary = Salary;
-                double GS = 0.00;
-                double NS = 0.00;
-                // double sal = this.Salary;
-                if (this.Salary < 5000)
-                {
-                    this.HRA = this.Salary * .10;
-                    this.TA = this.Salary * .05;
-                    this.DA = this.Salary * .15;
-                    double S = this.Salary;
-                    double H = this.HRA;
-                    double T = this.TA;
-                    double D = this.DA;
-                    this.GrossSalary = S + H + T + D;
-                    GS = this.GrossSalary;
-                    this.PF = (GS / 100) * 10;
-                    this.TDS = (GS / 100) * 18;
-                    this.NetSalary = GS - (this.PF + this.TDS);
-                    NS = this.NetSalary;
+            WriteLine("Enter Employee Details Like emp_id,emp_name");
 
-                }
-                else if (this.Salary < 10000)
-                {
-                    this.HRA = this.Salary * .15;
-                    this.TA = this.Salary * .10;
-                    this.DA = this.Salary * .20;
-                    double S = this.Salary;
-                    double H = this.HRA;
-                    double T = this.TA;
-                    double D = this.DA;
-                    this.GrossSalary = S + H + T + D;
-                    GS = this.GrossSalary;
-                    this.PF = (GS / 100) * 10;
-                    this.TDS = (GS / 100) * 18;
-                    this.NetSalary = GS - (this.PF + this.TDS);
-                    NS = this.NetSalary;
-                }
-                else if (this.Salary < 15000)
-                {
-                    this.HRA = this.Salary * .20;
-                    this.TA = this.Salary * .15;
-                    this.DA = this.Salary * .25;
-                    double S = this.Salary;
-                    double H = this.HRA;
-                    double T = this.TA;
-                    double D = this.DA;
-                    this.GrossSalary = S + H + T + D;
-                    GS = this.GrossSalary;
-                    this.PF = (GS / 100) * 10;
-                    this.TDS = (GS / 100) * 18;
-                    this.NetSalary = GS - (this.PF + this.TDS);
-                    NS = this.NetSalary;
-                }
-                else if (this.Salary < 20000)
-                {
-                    this.HRA = this.Salary * .25;
-                    this.TA = this.Salary * .20;
-                    this.DA = this.Salary * .30;
-                    double S = this.Salary;
-                    double H = this.HRA;
-                    double T = this.TA;
-                    double D = this.DA;
-                    this.GrossSalary = S + H + T + D;
-                    GS = this.GrossSalary;
-                    this.PF = (GS / 100) * 10;
-                    this.TDS = (GS / 100) * 18;
-                    this.NetSalary = GS - (this.PF + this.TDS);
-                    NS = this.NetSalary;
-                }
-                else if (this.Salary >= 20000)
-                {
-                    this.HRA = this.Salary * .30;
-                    this.TA = this.Salary * .25;
-                    this.DA = this.Salary * .35;
-                    double S = this.Salary;
-                    double H = this.HRA;
-                    double T = this.TA;
-                    double D = this.DA;
-                    this.GrossSalary = S + H + T + D;
-                    GS = this.GrossSalary;
-                    this.PF = (GS / 100) * 10;
-                    this.TDS = (GS / 100) * 18;
-                    this.NetSalary = GS - (this.PF + this.TDS);
-                    NS = this.NetSalary;
-                }
-                Console.WriteLine("\n Salary = " + this.Salary + "\n Emp Name = " + Ena + "\n Emp Number = " + Eno + "\n Emp Gross Salary = " + GS + "\n Emp net Salary = " + NS);
-            }
-            public void CalculateSalary()
-            {
+            emp_id = int.Parse(ReadLine());
+            emp_name = ReadLine();
+            Console.WriteLine("Enter Salary of an Employe .");
 
-            }
+            Salary = float.Parse(ReadLine());
 
         }
 
+        public virtual void CalculateSalary()
 
-        public static void Main(string[] arga)
         {
-            Console.Write("Enter Employee ID: ");
-            int ID = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Employee Name: ");
-            string Name = Console.ReadLine();
-            Console.Write("Enter Employee Salary: ");
-            double Salary = double.Parse(Console.ReadLine());
-            Employee emp = new Employee();
-            emp.setEmployee(ID, Name, Salary);
 
+        }
 
-            //Console.WriteLine( " Employee Number  =  " + emp.EmpNo + " \n Employee Name = " +  emp.EmpName + " \n Employee Salary = "+emp.Salary);
-            // Console.WriteLine(" Gross Salary = " + " \n PF = " + "\n Net Salary = ");
+    }
+    public class Manager : Employee, IPrintable
+
+    {
+
+        private float PA;
+
+        private float FA;
+
+        private float OA;
+
+        public override void CalculateSalary()
+
+        {
+
+            PA = 8 * Salary / 100;
+
+            FA = 13 * Salary / 100;
+
+            OA = 3 * Salary / 100;
+
+            GrossSalary = Salary + PA + FA + OA;
+
+        }
+
+        public void DisplayEmployeeDetails()
+
+        {
+
+            WriteLine("Manager Details...");
+
+            WriteLine("emp_id: {0}\nemp_name: {1}\nGrossSalary:{2}", emp_id, emp_name, GrossSalary);
+
+        }
+
+    }
+    public class MarketingExecutive : Employee, IPrintable
+    //inherited Employee class and interface IEmployee
+
+    {
+        private int Distance;
+        private float TourAllowances;
+        private float Telephone_Allowances;
+
+        public override void CalculateSalary() //override method of Employee class
+
+        {
+
+            WriteLine("Enter Distance travelled during Marketing Executive in km");
+
+            Distance = int.Parse(ReadLine());//taking distance input
+
+            TourAllowances = 5 * Distance; //5 rupees per km
+
+            Telephone_Allowances = 1000;
+
+            GrossSalary = Salary + TourAllowances + Telephone_Allowances;
+
+        }
+
+        public void DisplayEmployeeDetails() //implementation of interface method
+
+        {
+
+            WriteLine("Marketing Executive Details....");
+
+            WriteLine("emp_id: {0}\n emp_name: {1}\nGross Salary:{2}", emp_id, emp_name, GrossSalary);
+
+        }
+
+    }
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Manager mg = new Manager();
+
+            MarketingExecutive me = new MarketingExecutive();
+
+            WriteLine("Manager Input");
+
+            mg.GetSalaryWithEmployeeDetails();
+
+            mg.CalculateSalary();
+
+            mg.DisplayEmployeeDetails();
+
+            WriteLine("Marketing Executive Input");
+
+            me.GetSalaryWithEmployeeDetails();
+
+            me.CalculateSalary();
+
+            me.DisplayEmployeeDetails();
+
+            Console.ReadKey();
 
         }
     }
--- Assignment6/Assignment6/Program.cs
+++ Assignment6/Assignment6/Program.cs
@@ -1,2 +1,149 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace BankAccount
+
+{
+     class bank
+
+    {
+
+         double balance = 20000;
+
+        public double bal
+
+        {
+
+            get
+            {
+                return balance;
+              }
+
+            set
+            { 
+                balance = value;
+            }
+
+        }
+
+    }
+
+    class fuctions
+    {
+        bank i = new bank();
+        string name;
+
+        int account;
+
+        double withdraw, dep, tobal;
+        public void DepositorDetail()
+        {
+
+            Console.WriteLine("Enter Name of the depositor:");
+
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter Account Number of depositor:");
+
+            account = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Deposit Amount:");
+            dep = Convert.ToDouble(Console.ReadLine());
+            tobal = i.bal + dep;
+
+            Console.WriteLine("——————————\n");
+
+            Console.WriteLine("Name of the depositor: " +name);
+
+            Console.WriteLine("Account Number: " +account);
+
+            Console.WriteLine("Total Balance amount in the account: " +tobal);
+        }
+        public void CustomerDetail()
+        {
+
+            Console.WriteLine("Enter Account Name:");
+
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter Account Number:");
+
+            account = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Withdraw Amount:");
+
+            withdraw = Convert.ToDouble(Console.ReadLine());
+            if (withdraw <= i.bal)
+            {
+
+                tobal = i.bal - withdraw;
+
+                Console.WriteLine("____________\n");
+
+                Console.WriteLine("Account Name: " +name);
+
+                Console.WriteLine("Account Number: " +account);
+                Console.WriteLine("After Withdraw Amount balnace is : " +tobal);
+            }
+            else
+                Console.WriteLine("\n\nWithdraw Ammount does not Exist your Account.");
+        }
+    }
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+            char agn;
+            do
+            {
+
+                fuctions k = new fuctions();
+
+                int num;
+
+                Console.WriteLine("Please Select Any Function.");
+
+                Console.WriteLine("\nPress 1 for Deposit an Amount. \nPress 2 for Withdraw an Amount.") ;
+
+                num = Convert.ToInt32(Console.ReadLine());
+
+                switch (num)
+
+                {
+
+                    case 1:
+
+                        k.DepositorDetail();
+
+                        break;
+
+                    case 2:
+
+                        k.CustomerDetail();
+
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Invalid Selection!!!");
+
+                        break;
+
+                }
+
+                Console.WriteLine("\nDo you want to continue this program ? (y/n)");
+
+                agn =Convert.ToChar(Console.ReadLine());
+
+            } while (agn == 'y');
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
